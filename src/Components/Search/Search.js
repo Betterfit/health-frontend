ee3bcfea11d93afc6ef913b809e37be126811b65
Allison Boukall <allison.boukall@gmail.com>
2020-09-25 13:33:50 -0600

    Add in search component

---
+++ src/Components/Search/Search.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Search = () => (
+  <>
+    <label for="search"></label>
+    <div class="m-1 flex max-w-sm">
+      <div class="relative flex-grow">
+        <div class="absolute inset-y-0 left-0 pl-1 flex items-center pointer-events-none"></div>
+        <input
+          id="search"
+          class="form-input block w-full border border-gray-400 box-border pl-2 py-2 transition ease-in-out duration-150 text-lg"
+          placeholder="Search Products"
+        ></input>
+      </div>
+      <button class="-ml-px relative inline-flex items-center px-4 py-2 text-base leading-5 rounded-r uppercase font-medium text-white font-bold tracking-normal bg-gray-700 hover:bg-gray-600 focus:outline-none transition ease-in-out duration-150">
+        Search
+      </button>
+    </div>
+  </>
+);
+export default Search;
