fc7fb8c4290b4656845c43ebc2e0cce148e1bd07
Allison Boukall <allison.boukall@gmail.com>
2020-09-25 11:38:47 -0600

    Add in json for object and fix up mobile view

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,33 +1,68 @@
-import React from 'react'
-import Button from '../Button'
-import image from '../../Images/example_product.png'
-import Inventory_Description from '../Inventory/Inventory_Description'
-const Inventory = ({product}) => 
-    <div className="flex mb-4 bg-gray-100">
-        {product}
-        <div className="w-7/12 mx-2">
-            <h1 className="text-gray-700 text-xl font-semibold">NaCl 0.9% Minibag pk/4</h1>
-            <Inventory_Description title="Size" description="Example size" class_addons="pb-2"></Inventory_Description>
-            <Inventory_Description title="Description" class_addons="pb-2 pt-4" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras iaculis scelerisque enim, et venenatis arcu dictum nec. Praesent tristique lacinia nisi, pulvinar congue dui malesuada vitae."></Inventory_Description>
-            <div className ="w-1/2 grid grid-cols-2 py-8">
-                <h2 className ="text-gray-700 text-2-5xl font-semibold col-span-2"> Quantity</h2>
-                    <div className="col-span-1">                    
-                    <Inventory_Description title="Allotted" description="244" class_addons="pb-3"></Inventory_Description>
-                    </div>
-                    <div className="col-span-1">
-                    <Inventory_Description title="Available" description="244" edit={true}></Inventory_Description>
-                    </div>
-                {/*<Inventory_Description title="Allotted" description="244" clas></Inventory_Description>
-                {<Inventory_Description title="Available" description="244"></Inventory_Description>*/}
-            <div className="col-span-2 py-8">
-                <Button text="Save Changes" ></Button>
-        </div>
+import React from "react";
+import Button from "../Button";
+import image from "../../Images/example_product.png";
+import Inventory_Description from "../Inventory/Inventory_Description";
+
+//This Read Product and the attribute if exists, or a default value
+const Read_Product = (product_attr, default_value) => {
+  if (product_attr === undefined) {
+    return default_value;
+  }
+  return product_attr;
+};
+
+const Inventory = ({ product }) => {
+  return (
+    <>
+      <div className="mb-4 bg-gray-100 p-5">
+        <h1 className="text-gray-700 text-xl font-semibold">
+          {Read_Product(product.product_name, "")}
+        </h1>
+        <div className="flex lg:flex-row flex-col-reverse">
+          <div className="lg:w-7/12 pr-5">
+            <Inventory_Description
+              title="Size"
+              description={Read_Product(product.product_size, "N/A")}
+              class_addons="pb-2"
+            ></Inventory_Description>
+            <Inventory_Description
+              title="Description"
+              class_addons="pb-2 pt-4"
+              description={Read_Product(product.product_description, "")}
+            ></Inventory_Description>
+            <div className="w-1/2 lg:grid lg:grid-cols-2 py-8">
+              <h2 className="text-gray-700 text-2-5xl font-semibold col-span-2">
+                Quantity
+              </h2>
+              <div className="col-span-1">
+                <Inventory_Description
+                  title="Allotted"
+                  description={Read_Product(product.product_alloted, 0)}
+                  class_addons="pb-3"
+                ></Inventory_Description>
+              </div>
+              <div className="col-span-1">
+                <Inventory_Description
+                  title="Available"
+                  description={Read_Product(product.product_available, 0)}
+                  edit={true}
+                ></Inventory_Description>
+              </div>
+              <div className="col-span-2 py-8">
+                <Button text="Save Changes"></Button>
+              </div>
             </div>
-
+          </div>
+          <div className="lg:w-5/12 m-auto">
+            <img
+              className="border border-gray-400"
+              src={image}
+              alt="Betterfit Logo"
+            ></img>
+          </div>
         </div>
-        <div className="w-5/12 m-auto mx-2">
-            <img className = "border border-gray-400" src={image} alt="Betterfit Logo"></img>
-        </div>
-    </div>
-
-export default Inventory 
+      </div>
+    </>
+  );
+};
+export default Inventory;
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,12 +1,19 @@
 import React , {useState} from 'react'
 import Inventory from '../Components/Inventory/Inventory'
 
+let dummydata = { 
+    "product_name": "NaCl 0.9% Minibag pk/4",
+    "product_size": "100ml",
+    "product_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras iaculis scelerisque enim, et venenatis arcu dictum nec. Praesent tristique lacinia nisi, pulvinar congue dui malesuada vitae.",
+    "product_alloted": 244,
+    "product_available": 430,
+}
 const Dashboard = () => {
     // usestate to save user and pass
     return(
         <div>
             <h1>Im the dashboard page</h1>
-            <div className="w-10/12 flex place-self-center justify-self-center m-auto"><Inventory></Inventory></div>
+            <div className="w-10/12 flex place-self-center justify-self-center m-auto"><Inventory product={dummydata}></Inventory></div>
 
         </div>
     )
