196f8740c6aa98b1a032502745a7dcb1e0ca9d26
Tyler Olthuizen <tyler@liftinteractive.com>
2020-09-29 15:09:21 -0600

    side bar

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import SideBarNavigation from './SideBarNavigation';
-// import SideBarDashboardTypeCTA from './SideBarDashboardTypeCTA';
-// import SideBarProfile from './SideBarProfile';
+import SideBarDashboardTypeCTA from './SideBarDashboardTypeCTA';
+import SideBarProfile from './SideBarProfile';
 import logo from 'Images/logo.png'
 
 
@@ -21,24 +21,20 @@ const SideBar = () => {
       }
     ]
     return(
-        // <!-- Static sidebar for desktop -->
         <div class="md:flex md:flex-shrink-0">
           <div class="flex flex-col md:w-64">
-
             <div class="flex flex-col md:h-0 flex-1 border-r border-gray-400 bg-white">
               <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto px-4">
                 <div class="flex items-center flex-shrink-0">
                   <img class="w-48 md:w-3/4" src={logo} alt="Workflow"/>
                 </div>
-                {/* <SideBarDashboardTypeCTA name="Supplier Name" location="Edmonton,AB" /> */}
-                <SideBarNavigation navList={navItemsList} />
+                <SideBarDashboardTypeCTA name="Supplier Name" location="Edmonton,AB" /> 
+                <SideBarNavigation navList={navItemsList} /> 
               </div>
-              {/* TODO - DYNAMIC NAME ONCE API IS HOOKED UP */}
-              {/* <SideBarProfile userName="My Name" /> */}
+              <SideBarProfile userName="My Name" />
             </div>
           </div>
-        </div>
-        
+        </div>   
     )
 }
 
