ea9bb465d073e416f4c3a9b2062111ddef4227f8
Allison Boukall <allison.boukall@gmail.com>
2020-10-08 21:51:42 -0600

    Clean up code with prettifier

--- src/Components/Forms/FlatDetailButton.js
+++ src/Components/Forms/FlatDetailButton.js
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from "react";
 
-
 function FlatButton({ id_tag, text, onClick }) {
-
-    return (
-        <div className="relative">
-          <button
-            type="submit"
-            className={
-              "absolute rounded-md w-full flex justify-center py-3 border border-transparent font-semibold " +
-              " transition duration-150 ease-in-out capitalize text-base bg-betterfit-pale-blue border border-betterfit-basic-blue"
-            }
-          >
-            {text}
-          </button>
-          </div>
-      );
+  return (
+    <div className="relative">
+      <button
+        type="submit"
+        className={
+          "absolute rounded-md w-full flex justify-center py-3 border border-transparent font-semibold " +
+          " transition duration-150 ease-in-out capitalize text-base bg-betterfit-pale-blue border border-betterfit-basic-blue"
+        }
+      >
+        {text}
+      </button>
+    </div>
+  );
 }
 
 export default FlatButton;
