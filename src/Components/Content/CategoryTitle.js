f77e56d12d9e967493665e7cf8444f65a0e27934
Allison Boukall <allison.boukall@gmail.com>
2020-10-19 12:45:02 -0600

    Improve product count under category and add in images

--- src/Components/Content/CategoryTitle.js
+++ src/Components/Content/CategoryTitle.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const CategoryTitle = ({title, icon, background_color, color}) => {
-    const icon_color = (color!="" ? color : "#234499")
-    const svg = icon
-    const backgroundColor = {
-      background: (background_color!="" ? background_color : '#E4EFFC'),
-    };
-    return(
-        <div className="flex flex-row pb-4 pt-2 ">
-        <div className="rounded-full h-12 w-12 flex items-center mr-2 " style={backgroundColor}>
-        </div>
-        <h2 className="text-betterfit-graphite text-3xl">{title}</h2> 
-        </div>
-    )
-}
+const CategoryTitle = ({ title, icon, background_color, color }) => {
+  const outlinecolor = color != "" ? color : "#234499";
+  const svg = icon;
+  const backgroundColor = {
+    background: background_color != "" ? background_color : "#E4EFFC",
+  };
+  return (
+    <div className="flex flex-row pb-4 pt-2 ">
+      <div
+        className="rounded-full h-12 w-12 flex items-center mr-2 "
+        style={backgroundColor}
+      >
+        <img className="m-auto h-8 w-8" src={svg} />
+      </div>
+      <h2 className="text-betterfit-graphite text-3xl">{title}</h2>
+    </div>
+  );
+};
 
-export default CategoryTitle
+export default CategoryTitle;
--- src/Components/Order/CategoryCard.js
+++ src/Components/Order/CategoryCard.js
@@ -1,44 +1,49 @@
 import React, { useState } from "react";
-import CircleButton from "Components/Forms/CircleButton";
-import FlatButton from "Components/Forms/FlatDetailButton";
-import EmptyImage from "Images/emptyImage.png";
 import { ReactSVG } from "react-svg";
 import RightArrow from "Images/Icons/right-arrow.svg";
 import { useHistory } from "react-router-dom";
 
-
 const CategoryCard = (category) => {
-    const history = useHistory();
+  const history = useHistory();
   const getProductCount = (products) => {
     let sum = 0;
     products.forEach((val) => {
-        //if there are no product_variations, then 1 for the product
-      sum += (val.product_variations.length>0 ? val.product_variations.length : 1);
+      val.product_variations.forEach((product) => {
+        sum += product.product_options.length;
+      });
     });
     return sum;
   };
+
   const category_id = category.category.pk;
   const category_name = category.category.name;
   const count = getProductCount(category.category.products);
-  const color = (category?.category?.main_color!="" ? category.category.main_color : "#234499")
-  const svg = category.category.icon
+  const color =
+    category?.category?.main_color != ""
+      ? category.category.main_color
+      : "#234499";
+  const svg = category.category.icon;
   const backgroundColor = {
-    background: (category?.category?.background_color!="" ? category.category.background_color : '#E4EFFC'),
+    background:
+      category?.category?.background_color != ""
+        ? category.category.background_color
+        : "#E4EFFC",
   };
   return (
     <>
-      <div 
-      className="mb-2 p-6 rounded relative flex flex-row md:flex-col justify-content items-center rounded-lg border border-betterfit-light-blue hover:bg-betterfit-pale-blue hover:border-betterfit-basic-blue"
-      onClick={() => history.push(history.location.pathname + category_name+'/'+category_id)} >
+      <div
+        className="mb-2 p-6 rounded relative flex flex-row md:flex-col justify-content items-center rounded-lg border border-betterfit-light-blue hover:bg-betterfit-pale-blue hover:border-betterfit-basic-blue"
+        onClick={() =>
+          history.push(
+            history.location.pathname + category_name + "/" + category_id
+          )
+        }
+      >
         <div
           className="rounded-full h-18 w-18 flex items-center mr-2 "
           style={backgroundColor}
         >
-   <svg>
-      <use href={svg}>
-
-      </use>
-    </svg>
+          <img className="m-auto" src = {svg}/>
         </div>
         <div className="flex flex-col">
           <p className="font-semibold text-betterfit-graphite md:text-center text-base leading-tight md:pt-4">
