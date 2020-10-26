c41565b99d40348430eedbfbe397dd74d04739fd
Allison Boukall <allison.boukall@gmail.com>
2020-10-26 16:07:14 -0600

    Just run through linter

--- src/Components/TrafficControllerSideBar/ControllerCardStatus.js
+++ src/Components/TrafficControllerSideBar/ControllerCardStatus.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-
-
-const ControllerCardStatus = ({ value, textStyle, background, left=true }) => {
-    const rounded = (left ? 'rounded-l-full' : 'rounded-r-full');
+const ControllerCardStatus = ({
+  value,
+  textStyle,
+  background,
+  left = true,
+}) => {
+  const rounded = left ? "rounded-l-full" : "rounded-r-full";
   return (
     <div
       className={`uppercase py-2 px-3 text-xs ${background} ${textStyle} ${rounded}`}
