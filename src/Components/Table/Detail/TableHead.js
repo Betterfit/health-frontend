7d7e20776d4702829aec71dba2d5e2bdbcd7358f
Allison Boukall <allison.boukall@gmail.com>
2020-11-02 13:55:40 -0700

    Update key errors with table and get table to work with both tickets and order api

--- src/Components/Table/Detail/TableHead.js
+++ src/Components/Table/Detail/TableHead.js
@@ -4,9 +4,9 @@ const TableHead = ({TableHead}) => {
     return(
         <thead>
             <tr>
-                {TableHead.map(head => {
+                {TableHead.map((head,i)=> {
                     return(
-                        <th className="px-4 py-3  text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider uppercase text-gray-700 font-extrabold ">
+                        <th key ={i} className="px-4 py-3  text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider uppercase text-gray-700 font-extrabold ">
                             {head.replace(/[_-]/g, " ")}
                         </th>
                     )                 
