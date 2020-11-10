6b40b110a6fad20aa1d7130d4f88aa58315cee14
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-11-10 15:30:22 -0700

    created a useResources hook

---
+++ src/Helpers/useResources.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Resource, Tag } from "Types";
+import Api from "./api";
+
+type ResourceFilter = (resource: Resource) => boolean;
+
+/**
+ * This hook simplifies the fetching, searching and filtering of resources.
+ * @param customFilters A list of filters to apply in addition to search and tag matching
+ */
+const useResources = (customFilters: ResourceFilter[] = []) => {
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    // stores tag pks (primary key) rather than whole tag
+    const [selectedTags, setSelectedTags] = useState<number[]>([]);
+    const api = new Api();
+    const { data, isLoading: resourcesLoading } = useQuery<Resource[]>(
+        // the key is the combination of 'resources' and the search term, used for caching
+        ["resources", searchTerm],
+        async () =>
+            await api.getResources(searchTerm).then((resp: any) => resp.data),
+        { placeholderData: [], keepPreviousData: true }
+    );
+    const resources = data as Resource[];
+    // For each resource, check that it passes every custom filter specified.
+    const filteredResources: Resource[] = resources.filter((resource) =>
+        customFilters.every((filter) => filter(resource))
+    );
+    // filters out resources that don't match every selected tag
+    const tagMatchingResources = findResourcesMatchingTagPKList(
+        filteredResources,
+        selectedTags
+    );
+
+    const toggleTagSelect = (tag: Tag) => {
+        if (selectedTags.includes(tag.pk))
+            setSelectedTags(selectedTags.filter((tagPK) => tagPK === tag.pk));
+        else setSelectedTags([...selectedTags, tag.pk]);
+    };
+
+    return {
+        resources: tagMatchingResources,
+        resourcesLoading,
+        setSearchTerm,
+        searchTerm,
+        toggleTagSelect,
+        selectedTags,
+    };
+};
+
+const findResourcesMatchingTagPKList = (
+    resources: Resource[],
+    tagPKs: number[]
+): Resource[] => {
+    if (tagPKs.length === 0) return resources;
+    const matchingResources = [];
+    // not the nicest time complexity but tagPKs will normally be quite small
+    for (const resource of resources) {
+        for (const tag of resource.tags) {
+            if (tagPKs.includes(tag.pk)) matchingResources.push(resource);
+        }
+    }
+    return matchingResources;
+};
+
+export default useResources;
