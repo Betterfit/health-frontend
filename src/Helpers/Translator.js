5ca5e85b4cf62c18f0f8247ad35eeb9d710d8742
Allison Boukall <allison.boukall@gmail.com>
2020-11-13 14:33:08 -0700

    add in script to handle quick translation imports

--- src/Helpers/Translator.js
+++ src/Helpers/Translator.js
@@ -1,19 +1,16 @@
 import EnglishTranslations from 'Data/TranslationsEn.json';
 import FrenchTranslations from 'Data/TranslationsFr.json';
 import {useAuthStore} from "Context/authContext";
-const Translator = (word) =>{
+const Translator = (word) => {
     const authStore = useAuthStore();
     const language = authStore.language;
-    if(language == "en"){
-        return(
-            EnglishTranslations[word]
-        )
-    }else{ 
-        return(
-            FrenchTranslations[word] 
-        )
+    switch(language) {
+        case "en":
+            return EnglishTranslations[word.toLowerCase()];
+        case "fr":
+            return FrenchTranslations[word.toLowerCase()];
+        default:
+            return word;
     }
-
-
 }
 export default Translator;
