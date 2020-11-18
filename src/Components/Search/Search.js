b44a79b0c8a295a39f22a8c02d928229da327159
Allison Boukall <allison.boukall@gmail.com>
2020-11-18 10:35:24 -0700

    Update search text font and add in translations

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -32,8 +32,8 @@ const Search = ({type}) => {
                 <label htmlFor="search" aria-label="Search"></label>
                 <input
                   id="search"
-                  className="input-reset form-input block w-full box-border pl-2 py-2 transition ease-in-out duration-150 text-lg bg-transparent"
-                  placeholder="Search Products"
+                  className="input-reset form-input block w-full box-border pl-2 py-2 transition ease-in-out duration-150 text-base bg-transparent"
+                  placeholder={Translator("Search Orders")}
                   ref={searchRef}
                   onChange={()=>{
                     clearTimeout();
@@ -69,7 +69,7 @@ const Search = ({type}) => {
         <div className="relative flex-grow">
           <input
             id="search"
-            className="input-reset form-input block w-full box-border pl-2 py-2 transition ease-in-out duration-150 text-lg bg-transparent"
+            className="input-reset form-input block w-full box-border pl-2 py-2 transition ease-in-out duration-150 text-base bg-transparent"
             placeholder={Translator("Search Resources")}
             ref={searchRef}
             onChange={()=>{
@@ -90,8 +90,8 @@ const Search = ({type}) => {
         <label htmlFor="search" aria-label="Search"></label>
           <input
             id="search"
-            className="input-reset form-input block w-full box-border pl-2 py-2 transition ease-in-out duration-150 text-lg bg-transparent"
-            placeholder="Search Orders"
+            className="input-reset form-input block w-full box-border pl-2 py-2 transition ease-in-out duration-150 text-base bg-transparent"
+            placeholder={Translator("Search Products")}
             ref={searchRef}
             onChange={()=>{
               clearTimeout();
