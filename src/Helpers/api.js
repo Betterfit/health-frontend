2ea98f739de83bea0d3418538e66480d96cc5e14
Allison Boukall <allison.boukall@gmail.com>
2020-11-19 14:31:38 -0700

    Update api

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -5,7 +5,7 @@ export default class Api {
   constructor() {
     this.api_token = localStorage.getItem('token');
     this.client = null;
-    this.api_url = 'https://betterfit.health/'  
+    this.api_url = 'https://api.betterfit.health/'  
   }
 
   init = () => {
@@ -20,7 +20,7 @@ export default class Api {
 
 
     this.client = axios.create({
-        baseURL: 'https://betterfit.health/' ,
+        baseURL: 'https://api.betterfit.health/' ,
         timeout: 31000,
         headers: headers,
     });
