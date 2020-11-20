22e8ef62ef42e4634b5b07de3057f12557dac31e
Allison Boukall <allison.boukall@gmail.com>
2020-11-20 16:18:31 -0700

    Work out conflict between fixes for edit product details.

--- src/Containers/Supplier/Inner/DashboardInventory.js
+++ src/Containers/Supplier/Inner/DashboardInventory.js
@@ -121,7 +121,7 @@ const DashboardInventory = () =>{
                     <Route exact path='/dashboard/inventory/product/:id' exact render={(props) => {
                         return ( <DashboardProductList {...props } edit={true} /> )
                     }} />
-                    <Route path='/dashboard/inventory/product/:id/detail/:oid?' exact render={(props) => {
+                    <Route path='/dashboard/inventory/product/:id/detail/:oid?/edit' exact render={(props) => {
                         return ( <DashboardProductDetail edit={true} {...props } /> )
                     }} />
                     <Route path='/dashboard/inventory/search:query?'>
