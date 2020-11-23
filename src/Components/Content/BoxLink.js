fd0b9f7027553b7a21ce1730b46777bffc1913cc
Allison Boukall <allison.boukall@gmail.com>
2020-11-23 13:47:25 -0700

    remove border on inventory cards

--- src/Components/Content/BoxLink.js
+++ src/Components/Content/BoxLink.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import { ReactSVG } from 'react-svg';
 import RightLongArrow from 'Images/Icons/right-long-arrow.svg';
-const BoxLink = ({borderColor , textColor,link, to, id}) => {
+const BoxLink = ({textColor, link, to, id}) => {
     return(
         <NavLink to={`${to}${id}`} 
-          className={`border border-${borderColor} text-${textColor} px-4 py-5 flex justify-between bg-white rounded-md box-link font-bold`}
+          className={`text-${textColor} px-4 py-5 flex justify-between bg-white rounded-md box-link font-bold`}
         >
             {link}
             <ReactSVG src={RightLongArrow} className="flex items-center"  beforeInjection={(svg) => { svg.setAttribute('style', 'width: 16px;margin-left:15px;')}}  />
