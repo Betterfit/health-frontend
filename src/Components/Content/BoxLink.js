e9f82cf6b49782d532b305826629e0ad94821f26
Allison Boukall <allison.boukall@gmail.com>
2020-11-23 13:56:58 -0700

    push text to semibold on inventory cards

--- src/Components/Content/BoxLink.js
+++ src/Components/Content/BoxLink.js
@@ -5,7 +5,7 @@ import RightArrow from 'Images/Icons/right-arrow.svg';
 const BoxLink = ({textColor,link, to, id}) => {
     return(
         <NavLink to={`${to}${id}`} 
-          className={`text-${textColor} pl-4 pr-2 py-5 flex justify-between bg-white rounded-md box-link font-bold`}
+          className={`text-${textColor} pl-4 pr-2 py-5 flex justify-between bg-white rounded-md box-link font-semibold`}
         >
             {link}
             <ReactSVG src={RightArrow} className="flex items-center opacity-25"  beforeInjection={(svg) => { svg.setAttribute('style', 'width: 16px;margin-left:15px;')}}  />
