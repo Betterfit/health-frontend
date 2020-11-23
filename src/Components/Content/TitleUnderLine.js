921b44766902620dc85c140a1a61f74509b6ac15
Allison Boukall <allison.boukall@gmail.com>
2020-11-23 09:18:46 -0700

    Fix spacing between title and back navigation

--- src/Components/Content/TitleUnderLine.js
+++ src/Components/Content/TitleUnderLine.js
@@ -6,10 +6,10 @@ const TitleUnderLine = ({title,nounderline,extraclasses}) => {
     return(
         <>
         {nounderline && (
-            <h2 className={`text-dark-blue text-3xl py-4 pb-6 mb-6 ${extraclasses}`}>{Translator(title)}</h2>
+            <h2 className={`text-dark-blue text-3xl pb-6 mb-6 ${extraclasses}`}>{Translator(title)}</h2>
         )} 
         {!nounderline && (
-            <h2 className={`text-dark-blue text-3xl py-4 pb-6 border-b border-gray-400 mb-6 ${extraclasses}`}>{Translator(title)}</h2>
+            <h2 className={`text-dark-blue text-3xl pb-6 border-b border-gray-400 mb-6 ${extraclasses}`}>{Translator(title)}</h2>
         )} 
         </>   
     )
