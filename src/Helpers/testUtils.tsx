9cf249c1fcd9aac2b70563c572c5ccca723a40f7
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-12-12 23:43:04 -0700

    Created custom render function for testing
    Wraps component in a QueryClientProvider, which is required in v3

---
+++ src/Helpers/testUtils.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// query clients are required in the beta version of react-query.
+// we export this a replacement to react testing library's render so we can wrap components that use
+// the useQuery or useQueries hooks automatically in a QueryClientProvider.
+// If this is not done, they will throw errors
+// https://react-query-beta.tanstack.com/reference/QueryClientProvider
+const render = (component: React.ReactNode) => {
+    const queryClient = new QueryClient()
+    rtlRender(
+        <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
+    );
+};
+export { render };
