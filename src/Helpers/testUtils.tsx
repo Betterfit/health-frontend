ff83fa3e4bd19fb9d57df9469be05e1c36f38bdc
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-12-12 23:44:54 -0700

    updated tests to remove race conditions

--- src/Helpers/testUtils.tsx
+++ src/Helpers/testUtils.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render as rtlRender } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-// query clients are required in the beta version of react-query.
+// query clients are required in v3 of react-query.
 // we export this a replacement to react testing library's render so we can wrap components that use
 // the useQuery or useQueries hooks automatically in a QueryClientProvider.
 // If this is not done, they will throw errors
