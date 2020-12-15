fc0d240a592affb1c711b5f7263be54589adf547
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-12-14 17:28:50 -0700

    fixing eslint warnings and formatting 3

--- src/Components/Forms/Button.js
+++ src/Components/Forms/Button.js
@@ -1,49 +1,49 @@
+import Translator from "Helpers/Translator";
+import Plus from "Images/Icons/plus.svg";
 import React from "react";
 import { ReactSVG } from "react-svg";
-import Plus from "Images/Icons/plus.svg";
-import Translator from "Helpers/Translator";
 //To add an svg icon - add component to svg and add to svgMap
 //So if svgName = 'plus' will call the Plus component
 
-const PlusButton = () => (
-  <ReactSVG
-    src={Plus}
-    className=" px-2"
-    beforeInjection={(svg) => {
-      svg.setAttribute("style", "width:25px;height:25px;stroke:#ffffff;");
-    }}
-  />
+export const PlusButton = () => (
+    <ReactSVG
+        src={Plus}
+        className=" px-2"
+        beforeInjection={(svg) => {
+            svg.setAttribute("style", "width:25px;height:25px;stroke:#ffffff;");
+        }}
+    />
 );
 
 const Button = ({
-  text,
-  onClick,
-  solid = true,
-  text_size = "text-lg ",
-  color = " bg-betterfit-basic-blue",
-  hoverColor = "bg-betterfit-darker-blue",
-  textColor = "text-white ",
-  borderColor = "",
-  type = "submit",
+    text,
+    onClick,
+    solid = true,
+    text_size = "text-lg ",
+    color = " bg-betterfit-basic-blue",
+    hoverColor = "bg-betterfit-darker-blue",
+    textColor = "text-white ",
+    borderColor = "",
+    type = "submit",
 }) => {
-  let css_add_on = solid
-    ? "hover:" + hoverColor + " " + color
-    : "bg-transparent border border-betterfit-grey";
-  return (
-    <span className="block w-full shadow-sm flex items-baseline">
-      <button
-        onClick={onClick}
-        type={type}
-        className={`
+    let css_add_on = solid
+        ? "hover:" + hoverColor + " " + color
+        : "bg-transparent border border-betterfit-grey";
+    return (
+        <span className="block w-full shadow-sm flex items-baseline">
+            <button
+                onClick={onClick}
+                type={type}
+                className={`
           ${css_add_on}
           rounded-md w-full flex justify-center py-3 border border-transparent font-bold
           focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700
           transition duration-150 ease-in-out capitalize font-bold ${text_size} ${textColor} ${borderColor}`}
-      >
-        {Translator(text)}
-      </button>
-    </span>
-  );
+            >
+                {Translator(text)}
+            </button>
+        </span>
+    );
 };
 
 export default Button;
--- src/Components/Table/Full/TableHead.js
+++ src/Components/Table/Full/TableHead.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import uuid from 'react-uuid'
 import Translator from "Helpers/Translator";
+import React from "react";
+import uuid from "react-uuid";
 
-const TableHead = ({TableHead}) => {
-    return(
+const TableHead = ({ TableHead }) => {
+    return (
         <thead>
             <tr>
-                {TableHead.map((head, index) => {
-                    return(
-                        <th key={uuid()} className="px-4 pb-2 pt-6 text-left leading-4 uppercase font-semibold text-10 tracking-extra-wide text-gray-600 opacity-50">
+                {TableHead.map((head) => {
+                    return (
+                        <th
+                            key={uuid()}
+                            className="px-4 pb-2 pt-6 text-left leading-4 uppercase font-semibold text-10 tracking-extra-wide text-gray-600 opacity-50"
+                        >
                             {Translator(head)}
                         </th>
-                    )                 
+                    );
                 })}
-      
             </tr>
         </thead>
-    )
-}
+    );
+};
 export default TableHead;
