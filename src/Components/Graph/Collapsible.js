303deaedf1dda2b0fbad256d5338060a029c20cc
yash-bhandari <yashaswi@ualberta.ca>
2021-01-17 19:21:12 -0700

    filter fields easier to click

--- src/Components/Graph/Collapsible.js
+++ src/Components/Graph/Collapsible.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import Expand from "Images/Icons/expand.svg";
+import React, { useState } from "react";
 import { ReactSVG } from "react-svg";
 
 const Collapsible = ({ heading, children, onClickEvent }) => {
@@ -10,11 +10,11 @@ const Collapsible = ({ heading, children, onClickEvent }) => {
   };
 
   return (
-    <div className="bg-gray-300  w-6/12 rounded flex flex-col items-start justify-center p-2 border-2 border-white hover:bg-gray-400">
-      <div
-        className="w-full flex flex-row justify-between m-1 px-4"
-        onClick={collapsibleChangeIsOpen}
-      >
+    <div
+      className="cursor-pointer bg-gray-300 w-6/12 rounded flex flex-col items-start justify-center p-2 border-2 border-white hover:bg-gray-400"
+      onClick={collapsibleChangeIsOpen}
+    >
+      <div className="w-full flex flex-row justify-between m-1 px-4">
         <button className="text-betterfit-graphite text-sm">{heading}</button>
         <ReactSVG
           className={isOpen ? "transform rotate-180" : "transform rotate-0"}
@@ -28,9 +28,10 @@ const Collapsible = ({ heading, children, onClickEvent }) => {
         <div className="h-32 overflow-y-scroll overflow-x-hidden">
           <div className="rounded relative z-20border-transparent">
             <div className="bg-white">
-              {children.map((region) => {
+              {children.map((region, i) => {
                 return (
                   <button
+                    key={i}
                     className="graph__collapsible text-betterfit-grey-blue text-sm mx-1 text-left w-full px-2 hover:text-betterfit-basic-blue hover:font-extrabold"
                     onClick={(e) => onClickEvent(e, heading, region)}
                   >
