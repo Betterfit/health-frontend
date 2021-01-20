b987e607b93c4c031655850acc7f6cbf19e93af9
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2021-01-19 19:45:56 -0700

    added logo

--- src/Pages/CovidGraphPage.tsx
+++ src/Pages/CovidGraphPage.tsx
@@ -1,7 +1,9 @@
 import Button from "Components/Forms/Button";
 import InputField from "Components/Forms/Input_Field";
 import DashboardGraph from "Containers/Traffic/Inner/DashboardGraph";
+import Logo from "Images/logo.svg";
 import React, { useState } from "react";
+import { ReactSVG } from "react-svg";
 
 const fake_password =
   process.env.REACT_APP_GRAPH_PAGE_FAKE_PASSWORD || "canada";
@@ -12,8 +14,8 @@ const requirePassword =
 export const CovidGraphPage = () => {
   const [authorized, setAuthorized] = useState(!requirePassword);
 
-  let graphWidth = window.innerWidth * 0.5;
-  if (window.innerWidth < 600) {
+  let graphWidth = Math.min(window.innerWidth * 0.5, 1024);
+  if (window.innerWidth < 500) {
     graphWidth = window.innerWidth * 0.95;
   }
   const graphHeight = 0.8 * graphWidth;
@@ -21,10 +23,26 @@ export const CovidGraphPage = () => {
   if (!authorized) return <FakeLogin authorize={() => setAuthorized(true)} />;
 
   return (
-    <div className="w-full flex flex-col items-center">
-      <h1 className="self-center text-2xl text-dark-blue py-3">
-        BetterFit COVID-19 Aggregator
-      </h1>
+    <div className="flex flex-col m-auto items-center max-w-screen-xl">
+      <div className="flex items-end py-2 w-full justify-between">
+        <a
+          href="https://betterfit.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ReactSVG
+            src={Logo}
+            className="px-2"
+            beforeInjection={(svg) => {
+              svg.setAttribute("style", "margin:auto;");
+            }}
+            alt="Betterfit"
+          />
+        </a>
+        <h1 className="text-2xl text-dark-blue">COVID-19 Aggregator</h1>
+        {/* invisible, used to get logo left aligned and title centered */}
+        <div className="px-20 py-1" />
+      </div>
       <DashboardGraph width={graphWidth} height={graphHeight} />
     </div>
   );
