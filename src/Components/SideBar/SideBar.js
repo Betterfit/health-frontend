b88f304fc77f57c20712111092223f7a2a57092d
Yash <yash@betterfit.co>
2021-06-03 05:41:53 +0000

    gradient side bar

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -43,7 +43,12 @@ const SideBar = ({ navItemsList }) => {
   return (
     <div className="md:flex md:flex-shrink-0 sidebar">
       <div className="flex flex-col sidebar md:p-2 md:pr-0 w-full">
-        <div className="flex flex-col md:h-0 flex-1 bg-blue relative rounded-lg m-2 mb-0 md:m-0">
+        <div
+          className="flex flex-col md:h-0 flex-1 relative rounded-lg m-2 mb-0 md:m-0"
+          style={{
+            backgroundImage: "linear-gradient(120deg, #6D2CFF, #1332C7)",
+          }}
+        >
           <div className="flex-1 flex flex-col pt-8 pb-1 overflow-y-auto">
             <div className="sm:pl-8 px-4 border-white border-b border-opacity-25 mb-2">
               <div className="flex items-center flex-shrink-0 flex flex-row items-center justify-between">
