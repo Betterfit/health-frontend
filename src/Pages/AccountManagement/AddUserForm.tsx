f9db69cb3b6429174414ee7cf87552e6dd2d3c99
Yash <yash@betterfit.co>
2021-06-23 06:00:27 +0000

    merged

--- src/Pages/AccountManagement/AddUserForm.tsx
+++ src/Pages/AccountManagement/AddUserForm.tsx
@@ -2,29 +2,47 @@ import {
   FormControlLabel,
   Radio,
   RadioGroup,
-  TextField,
+  TextField
 } from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import { useUserFacilities } from "APIHooks/facilities";
 import { groupUsersByFacility, useMyProfile, useUsers } from "APIHooks/user";
 import PrettyButton from "Components/Forms/PrettyButton/PrettyButton";
-import React, { useState } from "react";
+import { validate } from "email-validator";
+import TypedAPI from "Helpers/typedAPI";
+import React, { FormEventHandler, useState } from "react";
+import { Facility } from "Types";
 import styles from "./AddUserForm.module.css";
 
+const api = new TypedAPI();
 interface AddUserFormData {
   emails: string[];
-  facility: string;
+  facility: Facility | null;
   isAdmin: boolean;
 }
 const AddUserForm = () => {
-  const facilitiesQuery = useUserFacilities();
+  const facilitiesQuery = useUserFacilities({
+    onSuccess: (data) => {
+      // populate facility field automatically
+      const facility = data.find((facility) => facility.isAdmin);
+      console.log(facility);
+      if (!formData.facility && facility)
+        setFormData({ ...formData, facility });
+    },
+  });
   const myProfileQuery = useMyProfile();
   const usersQuery = useUsers();
   const [formData, setFormData] = useState<AddUserFormData>({
     emails: [],
-    facility: "",
+    facility: null,
     isAdmin: false,
   });
+  const [errors, setErrors] = useState({ emails: "", facility: "" });
+  const facilities = facilitiesQuery.isSuccess
+    ? // you can only add users to facilities that you are an admin of
+      facilitiesQuery.data.filter((facility) => facility.isAdmin)
+    : [];
+
   const users = usersQuery.isSuccess ? usersQuery.data : [];
   const usersByFacility = groupUsersByFacility(users);
   const userOptions = users.filter((user) => {
@@ -32,8 +50,9 @@ const AddUserForm = () => {
     if (user.isOrganizationAdmin) return false;
     // don't suggest users that are already in the facility
     if (
-      formData.facility in usersByFacility &&
-      user.email in usersByFacility[formData.facility]
+      formData.facility &&
+      formData.facility.pk in usersByFacility &&
+      user.email in usersByFacility[formData.facility.pk]
     )
       return false;
     // we don't want the user's own email to come up as an option
@@ -42,25 +61,61 @@ const AddUserForm = () => {
     // otherwise this is the user we can suggest
     return true;
   });
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e: React.FormEvent) => {
+    e.preventDefault();
+    const facility = formData.facility;
+    if (!facility) {
+      setErrors({ ...errors, facility: "Pick a facility." });
+      return;
+    }
+    formData.emails.forEach((email) => {
+      const user = userOptions.find((user) => user.email === email);
+      if (user) {
+        api.addExistingUserToFacility(
+          user.email,
+          facility.url,
+          formData.isAdmin
+        );
+      } else {
+        api.addNewUserToFacility(email, facility.pk, formData.isAdmin);
+      }
+    });
+  };
   return (
-    <form className={styles.root}>
+    <form className={styles.root} onSubmit={onSubmit}>
       <span>
         <p>Add</p>
         <Autocomplete
           multiple
           freeSolo
-          onChange={(e, value) => console.log(value)}
+          value={formData.emails}
+          onChange={(e, emails) => {
+            console.log(emails);
+            if (!validate(emails[emails.length - 1])) {
+              setErrors({ ...errors, emails: "Invalid email" });
+              return;
+            }
+            setErrors({ ...errors, emails: "" });
+            setFormData({
+              ...formData,
+              emails: emails.filter((email) => validate(email)),
+            });
+          }}
           className="flex-1"
           id="email-input"
           aria-label="email"
-          options={userOptions}
-          getOptionLabel={(user) => user.email}
+          options={userOptions.map((user) => user.email)}
+          filterSelectedOptions
           renderInput={(params) => (
             <TextField
               {...params}
               label="Email"
               variant="outlined"
               size="small"
+              autoFocus
+              error={Boolean(errors.emails)}
+              helperText={errors.emails}
             />
           )}
         />
@@ -71,14 +126,21 @@ const AddUserForm = () => {
           className="flex-1"
           id="facility-input"
           aria-label="facility"
-          options={facilitiesQuery.isSuccess ? facilitiesQuery.data : []}
+          value={formData.facility}
+          options={facilities}
           getOptionLabel={(facility) => facility.name}
+          onChange={(e, facility) => {
+            setFormData({ ...formData, facility });
+            setErrors({ ...errors, facility: "" });
+          }}
           renderInput={(params) => (
             <TextField
               {...params}
               label="Facility"
+              error={Boolean(errors.facility)}
               variant="outlined"
               size="small"
+              required
             />
           )}
         />
@@ -105,6 +167,4 @@ const AddUserForm = () => {
     </form>
   );
 };
-const dummyEmails = ["yash@betterfit.co", "yashaswi@ualberta.ca"];
-const roles = ["Admin", "User"];
 export default AddUserForm;
