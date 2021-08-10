fca39ad5c36c3b69403dfb8116fa4ba52ae2a7bc
Yash-Bhandari <yash@betterfit.com>
2021-08-09 22:50:52 -0600

    cleaned up product details card

--- src/Components/Content/ProductDetailsCard.js
+++ src/Components/Content/ProductDetailsCard.js
@@ -2,47 +2,36 @@ import ProductImageCard from "Components/Content/ProductImageCard";
 import React from "react";
 import InventoryDescription from "../Inventory/Inventory_Description";
 
-//This will either return the attribute if it exists, or
-// return the passed in 'default_value' if not
-const Read_Product = (product_attr, default_value) => {
-  if (product_attr === undefined) {
-    return default_value;
-  }
-  return product_attr;
-};
-
 const ProductDetailCard = ({ product, children }) => {
   return (
-    <>
-      <div className="flex lg:flex-row flex-col-reverse">
-        <div className="xl:w-3/5 lg:w-1/2 lg:pr-12 py-4 mx-2">
+    <div className="flex lg:flex-row flex-col-reverse">
+      <div className="xl:w-3/5 lg:w-1/2 lg:pr-12 py-4 mx-2">
+        <InventoryDescription
+          title={product.product_label}
+          description={product.product_label_value ?? "N/A"}
+          class_addons="pb-2"
+        />
+        {product.product_category && (
           <InventoryDescription
-            title={product.product_label}
-            description={Read_Product(product.product_label_value, "N/A")}
-            class_addons="pb-2"
-          ></InventoryDescription>
-          {product.product_category && (
-            <InventoryDescription
-              title="Category"
-              class_addons="pb-2 pt-4"
-              description={Read_Product(product.product_category, "N/A")}
-            ></InventoryDescription>
-          )}
-          {product.product_description && (
-            <InventoryDescription
-              title="Description"
-              class_addons="pb-2 pt-4"
-              description={Read_Product(product.product_description, "N/A")}
-            ></InventoryDescription>
-          )}
-        </div>
-        <div className="xl:w-2/5 lg:w-1/2 py-4 mx-2">
-          <ProductImageCard image={product.product_image} name={product.name}>
-            {children}
-          </ProductImageCard>
-        </div>
+            title="Category"
+            class_addons="pb-2 pt-4"
+            description={product.product_category ?? "N/A"}
+          />
+        )}
+        {product.product_description && (
+          <InventoryDescription
+            title="Description"
+            class_addons="pb-2 pt-4"
+            description={product.product_description ?? "N/A"}
+          />
+        )}
       </div>
-    </>
+      <div className="xl:w-2/5 lg:w-1/2 py-4 mx-2">
+        <ProductImageCard image={product.product_image} name={product.name}>
+          {children}
+        </ProductImageCard>
+      </div>
+    </div>
   );
 };
 export default ProductDetailCard;
