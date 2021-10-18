205b3c59dfe4980428f33f19d4c1279ddc77c4e5
Yash-Bhandari <yash@bhandari.dev>
2021-10-18 14:41:37 -0600

    NewOrderHeader uses FacilitySelector

--- src/Components/Order/NewOrderHeader.tsx
+++ src/Components/Order/NewOrderHeader.tsx
@@ -1,7 +1,7 @@
+import FacilitySelector from "Components/FacilitySelector";
 import dayjs from "dayjs";
 import Translator from "Helpers/Translator";
-import { useUserFacilities } from "Models/facilities";
-import React, { useEffect } from "react";
+import React from "react";
 import { cartActions } from "Store/cartSlice";
 import { useAppDispatch, useAppSelector } from "Store/store";
 
@@ -31,17 +31,12 @@ const OrderComponentTitle = ({ title, value, classes }: TextProps) => {
  * Shows information about the order when editing.
  */
 const NewOrderHeader = () => {
-  const { data: userFacilities } = useUserFacilities();
   const destinationId = useAppSelector((state) => state.cart.destinationId);
   const orderId = useAppSelector((state) => state.cart.orderId);
   const dispatch = useAppDispatch();
   const setDestination = (facilityId: number) =>
     dispatch(cartActions.setDestinationId(facilityId));
   // sets the destination if it's currently undefined
-  useEffect(() => {
-    if (!destinationId && userFacilities && userFacilities.length > 0)
-      dispatch(cartActions.setDestinationId(userFacilities[0].id));
-  }, [destinationId, userFacilities, dispatch]);
   let orderDate = dayjs().format("MMM DD, YYYY");
   return (
     <div className="flex flex-col p-4 pb-2 border-b border-betterfit-grey border-opacity-40">
@@ -49,28 +44,11 @@ const NewOrderHeader = () => {
         title={orderDate}
         value={orderId ? `Order #${orderId}` : "New Order"}
       />
-      <div className="flex flex-col items-start">
-        <label
-          className="pl-1 uppercase text-betterfit-graphite text-xs tracking-extra-wide opacity-75 font-semibold"
-          htmlFor="facility"
-        >
-          Destination Facility
-        </label>
-        <select
-          name="facility"
-          className="bg-transparent text-lg"
-          onChange={(e) => {
-            setDestination(Number(e.target.value));
-          }}
-          value={destinationId}
-        >
-          {userFacilities?.map((facility) => (
-            <option key={facility.id} value={facility.id}>
-              {facility.name}
-            </option>
-          ))}
-        </select>
-      </div>
+      <FacilitySelector
+        label="Destination Facility"
+        facilityId={destinationId}
+        selectFacility={setDestination}
+      />
     </div>
   );
 };
