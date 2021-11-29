e70195bad6aa1a2248936799d8e221cdc1d13e22
Yash-Bhandari <yash@bhandari.dev>
2021-11-29 01:20:07 -0700

    simplified terms of service

--- src/Components/Content/TermsOfService.tsx
+++ src/Components/Content/TermsOfService.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import PrettyLink from "./PrettyLink";
-import styles from "./TermsOfService.module.css";
 
 const TermsOfService = () => {
   return (
-    <div className={styles.root}>
-      <p className={styles.title}>
-        Terms of Service{" "}
-        <PrettyLink
-          to="https://betterfit.com/wp-content/uploads/Website_Terms_of_Service_E9283890.pdf"
-          text="(View PDF)"
-        />
-      </p>
+    <div className="text-center">
+      <PrettyLink
+        to="https://betterfit.com/wp-content/uploads/Website_Terms_of_Service_E9283890.pdf"
+        text="View Terms of Service (PDF)"
+      />
     </div>
   );
 };
