c060586c41674f6bc7f7a203460b49b78fff9711
RishabBaisakh <rishab.baisakh@gmail.com>
2022-01-14 16:26:05 -0500

    available stock bug fix

--- src/Components/Order/OrderCart.tsx
+++ src/Components/Order/OrderCart.tsx
@@ -1,5 +1,4 @@
 import { AxiosError } from "axios";
-// import clsx from "clsx";
 import Dialog from "Components/Dialog";
 import FacilitySelector from "Components/FacilitySelector";
 import PrettyButton from "Components/Forms/PrettyButton/PrettyButton";
@@ -8,7 +7,6 @@ import ApproveOrderForm, {
 } from "Components/Order/ApproveOrderForm";
 import SignUpPrompt from "Components/SignUpPrompt";
 import { api } from "Helpers/typedAPI";
-// import { useSelectedFacility } from "Models/facilities";
 import { useOrder } from "Models/orders";
 import React, { useEffect, useState } from "react";
 import { useMutation, useQueryClient } from "react-query";
@@ -16,8 +14,6 @@ import { useHistory } from "react-router-dom";
 import { cartActions } from "Store/cartSlice";
 import { useAppDispatch, useAppSelector } from "Store/store";
 import CartItemList from "./CartItemList";
-// import ProductImage from "Components/Product/ProductImage";
-// import { HorizontalDetail } from "Components/InfoDisplay/LabeledDetails";
 
 const OrderCart = () => {
   const dispatch = useAppDispatch();
@@ -35,7 +31,6 @@ const OrderCart = () => {
   const [dialogOpen, setDialogOpen] = useState<
     null | "destination" | "approve" | "signup" | "outOfStock"
   >(null);
-  // const [outOfStockProducts, setOutOfStockProducts] = useState<any[]>([]);
 
   return (
     <>
@@ -50,60 +45,11 @@ const OrderCart = () => {
             facilityId={destinationId}
             selectFacility={setDestination}
           />
-          <CartActions
-            openDialog={setDialogOpen}
-            /* openOutOfStockList={(outOfStockProducts) => {
-              setOutOfStockProducts(outOfStockProducts);
-              setDialogOpen("outOfStock");
-            }} */
-          />
+          <CartActions openDialog={setDialogOpen} />
         </div>
         <CartItemList />
       </div>
       <Dialog open={dialogOpen != null} onClose={() => setDialogOpen(null)}>
-        {/* {dialogOpen === "outOfStock" && (
-          <div className="flex md:flex-col p-4">
-            <h1 className="text-xl text-center font-semibold text-betterfit-graphite mb-4">
-              Limited Stock
-            </h1>
-            {outOfStockProducts.map((product) => (
-              <>
-                <hr />
-                <div className="flex md:flex-row px-4">
-                  <ProductImage
-                    product={product.productOption}
-                    className={clsx(
-                      "h-20 w-20 md:h-32 md:w-32 pt-3 pr-3 object-contain"
-                      // outOfStock && styles.outOfStock
-                    )}
-                  />
-                  <div className="flex-col pt-7">
-                    <h1
-                      className={
-                        "text-base font-semibold text-betterfit-graphite"
-                      }
-                    >
-                      {product.productOption?.product}
-                    </h1>
-                    <span className="text-betterfit-grey-blue text-md">
-                      {product.productOption?.name
-                        ? product.productOption.name
-                        : "N/A"}
-                    </span>
-                    <HorizontalDetail
-                      label="Available Quantity"
-                      value={product.quantity}
-                    />
-                  </div>
-                </div>
-              </>
-            ))} 
-            <span className="text-xs text-red-500">
-              *Some of of product are limited or out stock. Change the selected
-              quantity in your cart.
-            </span>
-          </div>
-        )}*/}
         {dialogOpen === "approve" && (
           <ApproveOrderForm
             orderId={orderId!}
@@ -164,13 +110,10 @@ const useOrderMutation = (options: {
 
 const CartActions = ({
   openDialog,
-}: // openOutOfStockList,
-{
+}: {
   openDialog: (dialog: "approve" | "destination" | "signup") => void;
-  // openOutOfStockList: (outOfStockProducts: any) => void;
 }) => {
   const dispatch = useAppDispatch();
-  // const { facilityId } = useSelectedFacility();
 
   const cartItems = useAppSelector((state) => state.cart.items);
   const destinationId = useAppSelector((state) => state.cart.destinationId);
@@ -184,25 +127,6 @@ const CartActions = ({
       console.log(e);
     },
   });
-
-  /* const checkStock = () => {
-    const outOfStockProducts: any[] = [];
-    api.getInventory({ warehouse: facilityId }).then((products) => {
-      cartItems.forEach((item) => {
-        products.forEach((product) => {
-          if (
-            product.productOptionId === item.productOptionId &&
-            product.quantity < item.quantity
-          )
-            return outOfStockProducts.push(product);
-        });
-      });
-      outOfStockProducts.length === 0
-        ? orderMutation.mutate("open")
-        : openOutOfStockList(outOfStockProducts);
-    });
-    // orderMutation.mutate("open")
-  }; */
 
   const emptyCart = cartItems.length === 0;
   const needsDestination = loggedIn && !destinationId;
